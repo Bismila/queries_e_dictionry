1bd48c680f94670013b3ef3314ba152b30b2ee09
Bismila <you@example.com>
2022-07-01 17:13:08 +0300

    [*] delete comments

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-// import * as faker from 'faker';
 import {
   Chart as ChartJS,
   CategoryScale,
--- src/components/Errors.tsx
+++ src/components/Errors.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled';
 
-
 const ErrorContainer = styled.div`
    display: flex;
    flex-direction: row;
